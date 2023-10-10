515deb727c0e877539e613c7e2747cbc1643238c
virajnikam01 <virajnikam01@gmail.com>
2023-10-10 13:37:43 +0530

    fetched the individual details about the country on seperate page

--- script.js
+++ script.js
@@ -8,12 +8,14 @@ fetch("https://restcountries.com/v3.1/all")
       const countriesContainer = document.querySelector(".countries-container");
       const countryCard = document.createElement("a");
       countryCard.classList.add("country-card");
+      countryCard.href = `country.html?name=${country.name.common}`
+      
 
       countryCard.innerHTML = `
-<img src="${country.flags.svg}">
+<img src="${country.flags.svg}" alt="${country.name.common}">
 <div class="card-text">
     <h3 class="card-title">${country.name.common}</h3>
-    <p><b>Population :- </b>${country.population}</p>
+    <p><b>Population :- </b>${country.population.toLocaleString('en-IN')}</p>
     <p><b>Region :- </b>${country.region}</p>
     <p><b>Capital :- </b>${country.capital}</p>
 </div>
